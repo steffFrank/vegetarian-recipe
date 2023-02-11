f29aa44618977cb9f3c060eb9f7dd3937b8967ff
steffFrank <--replace-all>
2023-02-11 16:29:00 +0100

    refactor: Get the data with 2 differents funcitons

--- src/context/recipes.context.js
+++ src/context/recipes.context.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 import axios from "axios";
 
 const baseUrl = process.env.REACT_APP_BASEURL;
@@ -9,9 +9,14 @@ const resultsNumber = 30;
 export const RecipesContext = createContext({
     inputData: "", 
     url: "",
-    results: {
+    resultsRecipes: {
         isLoading: true,
         recipes: [],
+        error: null
+    },
+    resultsId: {
+        isLoading: true,
+        recipe: {},
         error: null
     }
 });
@@ -19,61 +24,81 @@ export const RecipesContext = createContext({
 export const RecipesProvider = ({children}) => {
     const [inputData, setInputData] = useState("");
     const [url, setUrl] = useState("");
-    const [results, setResults] = useState( {
+
+    const [resultsRecipes, setResultsRecipes] = useState( {
         isLoading: true,
         recipes: [],
         error: null
-    })
+    });
+    const [resultsId, setResultsId] = useState( {
+        isLoading: true,
+        recipe: {},
+        error: null
+    });
 
     const handleInputData = event => {
         setInputData(event.target.value);
     }
-
-    const createUrl = id => {
-        if(!inputData) {
-            return;
-        }
-        let url = "";
-        if (id !== "") {
-            url = `${baseUrl}${id}/information?/${apiQuery}`;
-        } else {
-            url = `${baseUrl}complexSearch?${apiQuery}&diet=${diet}&query=${inputData}&number=${resultsNumber}`;
-        }
-        setUrl(url);
-    }
     
-    useEffect(() => {
-        const getData = async (url) => {
-            try {
-                if (!url) return;
-                const response = await axios.get(url);
-                const data = response.data;
-                setResults(prevResults => {
-                    return {
-                        ...prevResults,
-                        isLoading: false,
-                        recipes: data.results
-                    }
-                })
-            } catch(error) {
-                setResults(prevResults => {
-                    return {
-                        ...prevResults,
-                        error: error,
-                        isLoading: false
-                    }
-                })
-            }
+    const getDataByInput = async () => {
+        if (!inputData) return;
+        const newUrl = `${baseUrl}complexSearch?${apiQuery}&diet=${diet}&query=${inputData}&number=${resultsNumber}`;
+        setUrl(newUrl);
+        try {
+            const response = await axios.get(newUrl);
+            const data = response.data.results;
+            setResultsRecipes(prevResults => {
+                return {
+                    ...prevResults,
+                    isLoading: false,
+                    recipes: data
+                }
+            });
+        } catch(error) {
+            setResultsRecipes(prevResults => {
+                return {
+                    ...prevResults,
+                    error: error,
+                    isLoading: false
+                }
+            })
         }
-        getData(url);
-    }, [url]);
+    }
+        
+
+    const getDataById = async (id) => {
+        if (!id) return;
+        const newUrl = `${baseUrl}${id}/information?${apiQuery}`;
+    
+        try {
+            const response = await axios.get(newUrl);
+            const data = response.data;
+            setResultsId(prevResults => {
+                return {
+                    ...prevResults,
+                    isLoading: false,
+                    recipe: data
+                }
+            });
+        } catch(error) {
+            setResultsId(prevResults => {
+                return {
+                    ...prevResults,
+                    error: error,
+                    isLoading: false
+                }
+            });
+        }
+    }
 
     const value = {
         handleInputData,
-        createUrl,
         inputData,
+        resultsRecipes,
+        getDataByInput,
+        getDataById,
         url,
-        results
+        resultsId
     };
 
     return (
