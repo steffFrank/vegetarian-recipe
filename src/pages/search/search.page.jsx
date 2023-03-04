2f00d43378cc0c114287311b1f89bf89998843c8
steffFrank <--replace-all>
2023-03-04 15:59:33 +0100

    feat: Remove useless props

--- src/pages/search/search.page.jsx
+++ src/pages/search/search.page.jsx
@@ -4,16 +4,13 @@ import { useContext } from "react";
 import { RecipesContext } from "../../context/recipes.context";
 
 export const Search = () => {
-    const { url } = useContext(RecipesContext);
-    const { recipesResult } = useContext(RecipesContext);
+   const { url } = useContext(RecipesContext);
+   const { recipesResult } = useContext(RecipesContext);
 
-    return (
-       <section>
-            <SearchForm />
-           {url &&  <RecipesPreview 
-                        recipesResult={recipesResult} 
-                        isButton={true}/> 
-           } 
-        </section>
-)
+   return (
+      <section>
+         <SearchForm />
+         {url && <RecipesPreview recipesResult={recipesResult} />}
+      </section>
+   );
 }
