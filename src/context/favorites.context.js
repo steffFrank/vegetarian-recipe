ca8f2863034b089f6e4a12e01e5d5ca3c7d21c9b
steffFrank <--replace-all>
2023-03-12 15:05:01 +0100

    refactor: Optimize code with reducer and memoization

--- src/context/favorites.context.js
+++ src/context/favorites.context.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState, useContext } from "react";
+import { createContext, useEffect, useContext, useReducer, useCallback, useMemo } from "react";
 import { RecipesContext } from "./recipes.context";
 
 
@@ -11,33 +11,69 @@ export const FavoritesContext = createContext({
     favoriteRecipes: []
 });
 
+const FAVORITE_ACTION_TYPES = {
+    GET_FAVORITE_RECIPES: "GET_FAVORITE_RECIPES",
+    TOGGLE_FAVORITE_RECIPE: "TOGGLE_FAVORITE_RECIPE"
+}
+
+const favoriteReducer = (state, action) => {
+    const {type, payload = {} } = action;
+
+    switch (type) {
+        case FAVORITE_ACTION_TYPES.GET_FAVORITE_RECIPES:
+            return {
+                favoriteRecipes: payload
+            }
+        case FAVORITE_ACTION_TYPES.TOGGLE_FAVORITE_RECIPE:
+            
+            const checkedRecipe = state.favoriteRecipes.find(favoriteRecipe => favoriteRecipe.id === payload.id);
+
+            let newFavorites = [...state.favoriteRecipes];
+
+            if (checkedRecipe) {
+                newFavorites = state.favoriteRecipes.filter(favoriteRecipe => favoriteRecipe.id !== payload.id);
+                payload.isFavorite = false;
+            } else {
+                newFavorites = [
+                    ...state.favoriteRecipes, 
+                    {
+                        ...payload, 
+                        isFavorite: true
+                    }
+                ];
+                payload.isFavorite = true;
+            }
+            return {
+                ...state,
+                favoriteRecipes: newFavorites
+            }
+        default:
+            throw new Error(`Unhandle type ${type} in favoriteReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    favoriteRecipes: getFavorites()
+}
+
 export const FavoritesProvider = ({children}) => {
-    const [favoriteRecipes, setFavoriteRecipes] = useState(getFavorites());
+    const [{ favoriteRecipes }, dispatch] = useReducer(favoriteReducer, INITIAL_STATE);
+
     const { updateRecipeFavoriteStatus } = useContext(RecipesContext);
 
    useEffect(() => {
     localStorage.setItem("favorites", JSON.stringify(favoriteRecipes));
    }, [favoriteRecipes]);
 
-   const toggleFavorites = (recipe) => {
-    const checkRecipe = favoriteRecipes.find(favoriteRecipe => favoriteRecipe.id === recipe.id);
+   const toggleFavorites = useCallback(recipe => {
+    dispatch({ type: FAVORITE_ACTION_TYPES.TOGGLE_FAVORITE_RECIPE, payload: recipe });
+    updateRecipeFavoriteStatus(recipe.id, !recipe.isFavorite);
+   }, [updateRecipeFavoriteStatus]);
 
-    if (checkRecipe) {
-        const removedFavorite = favoriteRecipes.filter(favoriteRecipe => favoriteRecipe.id !== recipe.id);
-        recipe.isFavorite = false;
-        setFavoriteRecipes(removedFavorite);
-        updateRecipeFavoriteStatus(recipe.id, false);
-    } else {
-        const newFavorites = [...favoriteRecipes, {...recipe, isFavorite: true}];
-        recipe.isFavorite = true;
-        setFavoriteRecipes(newFavorites);
-        updateRecipeFavoriteStatus(recipe.id, true);
-    }
-   }
-
-    const value = {
+    const value = useMemo(() => ({
         toggleFavorites,
         favoriteRecipes,
-    }
+    }), [toggleFavorites, favoriteRecipes]);
+    
     return <FavoritesContext.Provider value={value}>{children}</FavoritesContext.Provider>
 }
