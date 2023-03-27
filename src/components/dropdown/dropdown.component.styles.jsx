c7d1d94b93897054c18a51fc252c95b4453c45aa
steffFrank <--replace-all>
2023-03-27 16:37:45 +0200

    presentation

--- src/components/dropdown/dropdown.component.styles.jsx
+++ src/components/dropdown/dropdown.component.styles.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const StyledBar = styled.div`
     width: min(100%, 400px);
     margin: auto;
